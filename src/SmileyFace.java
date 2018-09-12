4511c59b7174c4ee5aba1516f6a59810656cc764
pk100101 <42978490+pk100101@users.noreply.github.com>
2018-09-12 14:08:45 -0400

    Add files via upload
    Here is my Smiley  Face Project

---
+++ src/SmileyFace.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Pragnya Kousik
+ *
+ */
+public class SmileyFace {
+
+	public static void main(String[] args) {
+		System.out.print("\\\\\\\\\\\\\n| *  * |\n|   *  |\n| \\__/ |\n \\_____/ ");
+		
+
+	}
+
+}
